76e6d5c2129336647700031bacdd25cb8c68de81
Leo Kallonen <leo.kallonen@gmail.com>
2017-01-12 12:19:26 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var express = require("express");
+/*
+ * Alustetaan sovellus, joka hyödyntää express-frameworkia
+ */
+var app = express();
+var expressWs = require('express-ws')(app);
+/*
+ * Sovelluksen käytössä resurssit, jotka löytyvät public-kansiosta
+ */
+app.use(express.static('public'));
+
+
+/*
+ * Routing
+ */
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.ws('/echo', function(ws, req) {
+    ws.on('message', function(msg) {
+        console.log(JSON.parse(msg).sender + " " + JSON.parse(msg).content);
+        expressWs.getWss().clients.forEach(function(client) {
+            client.send(msg);
+        });
+    });
+});
+
+/*
+ * Alustetaan palvelin
+ */
+var server = app.listen(8081, "0.0.0.0", function() {
+    var port = server.address().port;
+    console.log('Palvelin kuuntelee portissa ' + port);
+});
+
+
---
+++ public/js/chatController.js
@@ -0,0 +1,43 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+app.controller('chatController', function ($scope) {
+    var echoChannel = new WebSocket("ws://192.168.1.4:8081/echo");
+    
+    $scope.messageToServer = {
+        sender: "",
+        content: ""
+    };
+    
+    $scope.messagesToClient = [];
+    
+    function messageToClient(pSender, pContent) {
+        this.sender = pSender,
+        this.content = pContent
+    };
+
+    $scope.sendMessage = function () {
+        echoChannel.send(JSON.stringify($scope.messageToServer));
+        $scope.messageToServer.sender = "";
+        $scope.messageToServer.content = "";
+    };
+    
+    echoChannel.onopen = function() {
+        console.log("Connection opened.");
+    };
+    
+    echoChannel.onclose = function() {
+        console.log("Connection closed.");
+    };
+    
+    echoChannel.onmessage = function(evt) {
+        var parsedMessage = JSON.parse(evt.data);
+        var newMessage = new messageToClient(parsedMessage.sender, parsedMessage.content);
+        $scope.messagesToClient.push(newMessage);
+        $scope.$apply();
+        console.log($scope.messagesToClient);
+    };
+});
+
